3b6b2de89b442ad4cd91d428ba5e5a45520cb044
billalroar <billal775533@gmail.com>
2021-05-02 02:40:24 +0600

    Delete Weather directory

--- Weather/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Forecast
-
-
-class ForecastSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Forecast
-        fields = ["id", "timestamp", "temperatuer", "description","city"]
--- Weather/weather_scheduler/weather_updater.py
+++
@@ -1,8 +0,0 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-from Weather.views import WeatherViewset
-
-def start():
-  scheduler = BackgroundScheduler()
-  weather = WeatherViewset()
-  scheduler.add_job(weather.save_weather_data(), "interval", minutes=1,id="weather_001",replace_existing=True)
-  scheduler.start()
